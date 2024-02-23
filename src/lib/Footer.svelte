a1889d07f5b9ffa17e8e10cb0f6b28c083f32d99
Li Zhe <lizhe2004@gmail.com>
2024-02-23 14:05:52 +0800

    change model info

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -6,7 +6,7 @@
 	class="max-w-4xl mx-auto w-full flex md:flex-row flex-col items-center md:justify-between py-8 mt-20 border-t border-white/20"
 >
 	<div class="text-white/70 flex items-center flex-none">
-		Powered by <span class="mx-1 font-bold"> OpenAI</span>
+		Powered by <span class="mx-1 font-bold">阿里大模型</span>
 
 	</div>
 	<div class="flex items-center mt-2 md:mt-0">
